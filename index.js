8848288026c1863bcb85740be3359ea76d4ee46a
IngYerSeok <bdg97119@gmail.com>
2015-05-19 01:09:47 +0900

    test

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 var static = require('node-static');
 var http = require('http');
+var path = require('path');
+var express = require('express');
 
 var file = new(static.Server)();
 
-var app = http.createServer(function (req, res) {
-	file.serve(req, res);
-}).listen(process.env.PORT || 5000);
+var app = module.exports = express();
+var server = require('http').createServer(app);
+
+app.set('port', (process.env.PORT || 5000));
+app.set('views', __dirname + '/views');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+    res.render('index.html');
+});
+
+server.listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
