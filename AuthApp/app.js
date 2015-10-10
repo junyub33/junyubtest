cb146b4f092eee9a2ba7df42359824cd374f1629
junyub <junyub33@hanmail.net>
2015-10-11 02:54:50 +0900

    hhhh

---
+++ AuthApp/app.js
@@ -0,0 +1,118 @@
+
+var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
+
+var FACEBOOK_APP_ID = 'your app id';
+var FACEBOOK_APP_SECRET = 'your app secret';
+
+
+
+var ejs=require('ejs');
+var http = require('http');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
+
+
+app.get('/', function(req, res, next) {
+  res.sendfile('./html/auth.html');
+});
+
+
+passport.use(new FacebookStrategy({
+  clientID: 1178288362187545,
+  clientSecret: "34cec9637a77c6e91529490be3e6bfdf",
+  callbackURL: 'http://localhost:3000/auth/facebook/callback'
+}, function(accessToken, refreshToken, profile, done) {
+  process.nextTick(function() {
+    //Assuming user exists
+    done(null, profile);
+  });
+}));
+
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+  successRedirect: '/success',
+  failureRedirect: '/error'
+}));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+var server = require('http').createServer(app);
+
+  
+http.createServer(app).listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + app.get('port'));
+});
--- index.js
+++ index.js
@@ -2,6 +2,37 @@ var static = require('node-static');
 var http = require('http');
 var path = require('path');
 var express = require('express');
+var mysql = require('mysql');
+var bodyparser = require('body-parser');
+var ejs=require('ejs');
+var logger = require('morgan');
+
+var passport = require('passport');
+var FacebookStrategy = require('passport-facebook').Strategy;
+
+var FACEBOOK_APP_ID = '1639866069586311';
+var FACEBOOK_APP_SECRET = 'ea13af243934019780be8e42bcdd4f15';
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+
+// var router = require('router')
+
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'wjswktkwjs5',
+  database : 'psychodrama'
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('mysql connection error');
+        console.error(err);
+        throw err;
+    }
+});
 
 var file = new(static.Server)();
 
@@ -11,13 +42,205 @@ var server = require('http').createServer(app);
 app.set('port', (process.env.PORT || 5000));
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
+
 app.set('view engine', 'html');
+app.set('view engine', 'jade');
+
 app.use(express.static(__dirname + '/public'));
+app.use(logger('dev'));
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended:false})); //bodyparser 에 접근하기위해서
+
+
+
+app.use('/users', users);
+
 
 app.get('/', function(req, res){
-    res.render('index.html');
+  res.render('index.html');
 });
+
+app.get('/about', function(req, res) {
+  res.render('about.html');
+  
+});
+
+app.get('/start', function(req, res) {
+  res.render('start.html');
+  
+});
+
+app.get('/contact', function(req, res) {
+  res.render('contact.html');
+  
+});
+
+app.get('/popup', function(req, res) {
+  res.render('popup');
+  
+});
+
+app.get('/login', function(req, res) {
+  res.render('login.html');
+});
+
+app.get('/login1', function(req, res) {
+  res.render('login1.html');  
+  
+});
+
+app.get('/join', function(req, res) {
+  //console.log(req);
+  res.render('join.html');
+  
+});
+
+app.post('/user', function (req, res){
+  console.log(req);
+  console.log('login OK');
+
+
+
+  var user = {'id':req.body.id, 'name1':req.body.name1, 'password':req.body.password, 'year':req.body.year
+,'mon':req.body.mon,'date':req.body.date,'gender':req.body.gender,'email1':req.body.email1,'email2':req.body.email2,
+'tel1':req.body.tel1,'tel2':req.body.tel2,'tel3':req.body.tel3};
+  var query = connection.query('insert into user set ?', user, function(err,result){
+    if (err) {
+            console.error(err);
+            throw err;
+        }
+        res.render('join.html');
+    });   
+});
+
+
+app.get('/user', function(req,res){
+    var query = connection.query('select * from user',function (err,rows){
+        console.log(rows);
+        res.json(rows);
+    });
+    
+});
+
+app.post('/user/chk', function(req, res) {
+  console.log('chk ok');
+  var id = {'id':req.body.id};
+  var query = connection.query('select id from user where id = ?', req.body.id, function (err, result) {
+    if (err)  {
+      console.error(err);
+      throw err;
+    }
+    else {
+      if(result[0] != null) {
+        res.json({ "status" : 0 });
+      }
+      else {
+        res.json({ "status" : 1 });
+      }
+    }
+  });
+});
+
+app.post('/user/login', function(req, res) {
+  console.log('login ok');
+  var id = {'id':req.body.id, 'password':req.body.password};
+  var query = connection.query('select id from user where id = ?', req.body.id, function (err, result) {
+    if (err)  {
+      console.error(err);
+      throw err;
+    }
+    else {
+      if(result[0] != null) {
+        res.json({ "status" : 0 });
+      }
+      else {
+        res.json({ "status" : 1 });
+      }
+    }
+  });
+});
+
+
+
+
+
+
+
+
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
+
+
+app.get('/test', function(req, res, next) {
+  res.render('auth.html');
+});
+
+
+passport.use(new FacebookStrategy({
+  clientID: 1639866069586311,
+  clientSecret: "ea13af243934019780be8e42bcdd4f15",
+  callbackURL: 'http://localhost:5000/auth/facebook/callback'
+}, function(accessToken, refreshToken, profile, done) {
+  process.nextTick(function() {
+    //Assuming user exists
+    done(null, profile);
+  });
+}));
+
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+app.get('/auth/facebook/callback', passport.authenticate('facebook', {
+  successRedirect: '/success',
+  failureRedirect: '/error'
+}));
+
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj);
+});
+
+
+
+
+
 
 server.listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
+
+
+
 });
+
+  
